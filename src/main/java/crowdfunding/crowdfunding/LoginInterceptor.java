b673f5f750b533dc00325fe6db2a1cbd206f090d
Jo-Gwanghyun <mhc0404@naver.com>
2023-10-19 17:04:57 +0900

    세션

---
+++ src/main/java/crowdfunding/crowdfunding/LoginInterceptor.java
@@ -0,0 +1,22 @@
+package crowdfunding.crowdfunding;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class LoginInterceptor  implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                             Object handler) throws Exception {
+
+        String request = httpServletRequest.getRequestURI();
+
+        HttpSession session = httpServletRequest.getSession(false);
+        if(session == null || session.getAttribute("loginSession") == null){
+            httpServletResponse.sendRedirect("/user/login");
+            return false;
+        }
+        return true;
+    }
+}
